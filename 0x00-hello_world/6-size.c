e73e70eff0b8fde2e32fad4cde573be19d3ef59b
Nancy <nancykubania2@gmail.com>
2023-02-19 12:36:20 -0800

    0-positive_or_negative.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include <stdio.h> 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("size of a char: %d byte(s), size of (char));
+	printf("size of an int: %d byte(s), size of (int));
+	printf("Size of a long int: %d byte(s), size of (long int));
+	printf("Size of a long long int: $d byte(s) size of (long long int));
+      	return (0);
+}  
