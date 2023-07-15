32c0c91f68c61920af8fa3345c963958bb825804
Nancy <nancykubania2@gmail.com>
2023-07-14 21:25:08 -0700

    print using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - A program to print string using puts
+ * Return: 0 if successful
+ */
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
 return (0);
