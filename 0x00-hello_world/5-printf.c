00ea60e395d7dfbe6150ac6dcc40ca122bbdac71
Nancy <nancykubania2@gmail.com>
2023-07-14 21:32:45 -0700

    print using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /** main - A program that prints using printf
- * Returns 0 if successful
+ * Return: 0 if successful
  */
 int main(void)
 {printf("with proper grammar, but the outcome is a piece of art,\n");
