51270409e3bf5b33a51685220ea21eb70e17109f
Nancy <nancykubania2@gmail.com>
2023-07-17 07:57:50 -0700

    a program that prints alphabets using putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - A program that prints lowercase alphabetic numbers using putchar
+ * Return: Always 0 (sucess)
+ */
+
+int main(void)
+
+	ch = 'a'
+
+	while (ch <= z)
+	{
+		putchar('a');
+			a++;
+	return (0);
+	}
