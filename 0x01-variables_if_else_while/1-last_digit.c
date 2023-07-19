9498456ddca294429ea6e21213feac750f1f3cd1
Nancy <nancykubania2@gmail.com>
2023-07-19 03:39:58 -0700

    print random number if less than zero,greater than 5 and less than 6

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,17 +14,17 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	ld = n % 10;
-	if (n > 5)
+	if (ld > 5)
 	{
-		printf("Last digit of n is %d  and is greater than 5\n", n % 10);
+		printf("Last digit of %d is %d  and is greater than 5\n", n, ld);
 	}
 	else if (n < 6 && n != 0)
 	{
-		printf("Last digit of n is %d and is less than 6 and not 0\n", n % 10);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 	}
 	else
 	{
-		printf("Last digit of n is %d and is zero\n", n % 10);
+		printf("Last digit of %d is %d and is zero\n", n, ld);
 	}
 	return (0);
 }
