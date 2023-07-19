1a387e5dfa23546f7a0c71b23ba99d3b9bc8774d
Nancy <nancykubania2@gmail.com>
2023-07-19 03:22:30 -0700

    last digit of a random number if less than 6 greater than 5 and if 0

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,9 +9,11 @@
 int main(void)
 {
 	int n;
+	int ld;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	ld = n % 10;
 	if (n > 5)
 	{
 		printf("Last digit of n is %d  and is greater than 5\n", n % 10);
