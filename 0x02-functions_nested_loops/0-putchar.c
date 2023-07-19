83df450c9c75f583d5840d23a3eb875611734534
Nancy <nancykubania2@gmail.com>
2023-07-18 22:05:39 -0700

    print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - A program that prints putschar
@@ -6,8 +6,7 @@
  */
 int main(void)
 {
-	{
-		printf("_putchar\n")
-	}
-	return (0)
+	_putchar("_putchar");
+	_putchar("\n");
+	return (0);
 }
