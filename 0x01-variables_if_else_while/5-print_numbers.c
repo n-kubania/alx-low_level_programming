d51dbff7c1cb21040a18386005a233f8c33e8a29
Nancy <nancykubania2@gmail.com>
2023-07-20 10:51:31 -0700

    print numbers of base 10

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -4,11 +4,12 @@
  * Return: 0 Always (success)
  */
 
-int main()
+int main(void)
 {
-	for (int i = 0; i < 10; i++)
+	for (int n = 0; n < 10; n++)
 	{
-		printf("%d\n", i);
+		printf("%d", n);
 	}
+	printf("\n");
 	return (0);
 }
