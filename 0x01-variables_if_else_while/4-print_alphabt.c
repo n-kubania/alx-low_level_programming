0ab8fafc81292a274251f8128c998d93c7317dd3
Nancy <nancykubania2@gmail.com>
2023-07-20 01:35:36 -0700

    print alphabets except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,11 +7,13 @@
 int main(void)
 {
 	int n = 97;
-	
-	while ((n <= 122) n != 113 && n != 101))
+
+	while ((n <= 122) && (n != 113) && (n != 101))
+
 	{
 		putchar(n);
 		n++;
 	}
 	putchar('n');
 	return (0);
+}
