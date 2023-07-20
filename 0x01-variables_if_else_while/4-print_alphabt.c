94bdcb23448fe429243053c4cc403f1178dabd38
Nancy <nancykubania2@gmail.com>
2023-07-20 01:05:42 -0700

    file edit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - A program that prints all alphabets in lower case apart from q and e
+ * Return: 0 Always (success)
+ */
+
+int main(void)
+{
+	int n = 97;
+	
+	while ((n <= 122) n != 113 && n != 101))
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('n');
+	return (0);
