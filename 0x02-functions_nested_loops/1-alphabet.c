9ac42b8b3771e5081eed83d530fbefad8f9505b6
Nancy <nancykubania2@gmail.com>
2023-07-21 10:22:18 -0700

    print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - A program that prints alphabets in lowercase
+ * Return: 0 always (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
