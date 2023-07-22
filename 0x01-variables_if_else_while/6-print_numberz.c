f21b46d0a6cfc12fdc5f35ef2f00338415484231
Nancy <nancykubania2@gmail.com>
2023-07-22 13:18:04 -0700

    print numbers of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- prints numbers of base 10
+ * Return: 0 Always (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
