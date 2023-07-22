7c4d2b225953bf7ea54515cdb7b7c687571969ff
Nancy <nancykubania2@gmail.com>
2023-07-22 12:03:10 -0700

    print a line to standerror

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,9 @@
  * main - A program that prints a line to the standard error
  * return: 1 if successful
  */
+
+int main(void)
 {
-write (stdout, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-return (1);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
 }
