a006b94cf14c3dc450d2a0b0da54b92035fcbc3b
Nancy <nancykubania2@gmail.com>
2023-07-22 08:47:06 -0700

    prints ever minute of Jack Buer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59
+ * Return:void
+ */
+
+void jack_bauer(void)
+{
+	int p, q, r, s
+
+	for (p = 0; p <= 2; p++)
+	{
+	for (q = 0; q <= 9; q++)
+	}
+	if ((p <= 1 && q <= 9) || (p <= 2 && q <= 3))
+	}
+	for (r = 0; r <= 5; r++)
+	}
+	for (s = 0: s <= 9; s++)
+	}
+	_putchar(p + '0');
+	_putchar(q + '0');
+	_putchar(58)
+	_putchar(r + '0');
+	_putchar(s + '0');
+	_putchar('\n');
+}
