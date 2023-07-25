1f3fab43568376453b7569ad94fd57bd98449fab
Nancy <nancykubania2@gmail.com>
2023-07-25 13:39:13 -0700

    prints all characters of a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,15 +9,13 @@
 
 void puts2(char *str)
 {
-	int i;
+	unsigned int i;
 
 	if (str == NULL)
 	return;
 	
 	for (i = 0; str[i] != '\0'; i += 2)
 	{	
-		if ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
-	
 		putchar(str[i]);
 	}
 	putchar('\n');
