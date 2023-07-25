06e0a84c18400675afff7bd7ae46f7be8b7602a0
Nancy <nancykubania2@gmail.com>
2023-07-25 04:15:28 -0700

    n

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,15 @@
 
 void puts2(char *str)
 {
-	int i; 
+	int i;
 
 	if (str == NULL)
-        return;
-
+	return;
+	
 	for (i = 0; str[i] != '\0'; i += 2)
-	{
+	{	
+		if ((str[i] >= 'A' && str[i] <= 'Z') || (str[i] >= 'a' && str[i] <= 'z'))
+	
 		putchar(str[i]);
 	}
 	putchar('\n');
