fe507883a7c0a2fc5d9701c2c68e2d0800e4addc
Nancy <nancykubania2gmail.com>
2023-07-29 15:14:59 +0000

    n

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *reverse_array -reverses content of array
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void print_array(int *a, int n)
+{
+	int s1, s2, c;
+
+	while s2 
