9c93acdd0f97fd2923394f42b3a095e21e18382f
Nancy <nancykubania2@gmail.com>
2023-08-04 12:15:22 +0000

    multiplying two numbers

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -13,7 +12,7 @@ int main(int argc, char *argv[])
 {
 	int x, y, result;
 	(void)argc;
-	if (argc != 2)
+	if (argc != 3)
 	{
 		printf("error\n");
 		return (1);
