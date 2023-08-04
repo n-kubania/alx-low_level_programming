aa55ab3bce6969ae77c6488f6171d3bcac070ca8
Nancy <nancykubania2@gmail.com>
2023-08-04 11:24:56 +0000

    multiply two numbers

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
   * main-  A program that prints its name
@@ -11,6 +12,13 @@
 int main(int argc, char *argv[])
 {
 	(void)argc;
-	printf("%s\n", argv[0]);
+	program_name = strrchr(argv[0], '/');
+
+	if (argv[0] != NULL)
+		program_name++;
+	}
+	else if (program_name == argv[0])
+	{
+	printf("%s\n", program_name);
 	return (0);
 }
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main- A program that multiplies two numbers
+  * @argv: An array of string containing number of arguements
+  * @argc: Number of command line arguements
+  * Return: Always 0 (success)
+  */
+
+int main(int argc, char *argv[])
+{
+	int x, y, result;
+	(void)argc;
+	if (argc != 2)
+	{
+		printf("error\n");
+		return (1);
+	}
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	result = x * y;
+
+	{
+		printf("%d\n", result);
+	return (0);
+	}
+}
