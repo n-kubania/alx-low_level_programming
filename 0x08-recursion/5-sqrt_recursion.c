a78ec4786c7d41a1a4d301dfb5e19da015046b94
Nancy <nancykubania2@gmail.com>
2023-08-04 03:34:50 +0000

    natural square root of a number recursively

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * square -find out square of a number
- * @n: the number to find square root
- * @num: the number to be squared to get n
- * Return: square
+ * square- A function to find out square of a number
+ * @num: The number to be squared to get n
+ * @n: The number to find square root of
+ * Return: Square of n otherwise return -1
  */
 
-int square (int n, int num);
+int square(int n, int num);
 
 int _sqrt_recursion(int n)
 
@@ -16,12 +16,14 @@ int _sqrt_recursion(int n)
 }
 
 /**
- * _sqrt_recursion - A function that returns natural square root of a number
- * @n: the number
- * Return: Return 0 if square root is found otherwise return -1
+ * _sqrt_recursion- A function that returns natural square root of a number
+ * square- A function to find out square of a number
+ * @n: The number
+ * @num: The number to be squared
+ * Return: Return square root of n if square root is found otherwise return -1
  */
 
-int square (int n, int num)
+int square(int n, int num)
 {
 	if (num * num == n)
 	{
