89e0923c46b6f9f9e880b55d1200f4ab69c51d2b
Nancy <nancykubania2@gmail.com>
2023-08-10 10:02:40 +0000

    allocate memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -9,12 +9,12 @@
  */
 void *malloc_checked(unsigned int b)
 {
-    void *memory = malloc(b);
+	void *memory = malloc(b);
 
-    if (memory == NULL)
-    {
-        exit(98);
-    }
+	if (memory == NULL)
+	{
+	exit(98);
+	}
 
-    return memory;
+	return (memory);
 }
