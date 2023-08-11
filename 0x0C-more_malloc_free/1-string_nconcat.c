e9431daf4bc91983cb26919c25e7f43080897607
Nancy <nancykubania2@gmail.com>
2023-08-11 04:18:53 +0000

    allocates memory to an array

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -43,8 +43,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (j = 0; j < n; j++)
 	s[i + j] = s2[j];
 
-	/* Null-terminate the concatenated string */
 	s[i + j] = '\0';
-
 	return (s);
 }
