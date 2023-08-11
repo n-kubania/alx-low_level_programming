3f2340fe1ad8fde2bc8d56853f13d8048820bd3b
Nancy <nancykubania2@gmail.com>
2023-08-11 16:02:14 +0000

    array of integers

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * _realloc - Reallocate a memory block using malloc and free.
@@ -11,11 +12,13 @@
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-    void *new_ptr;
-    unsigned int i;
-    
+    void *ptr2;
+    unsigned int i, size;
+    char *char_ptr1, *char_ptr2;
 
-    if (new_size == 0 && ptr != NULL) {
+    /* Check conditions */
+    if (new_size == 0 && ptr != NULL)
+    {
         free(ptr);
         return NULL;
     }
@@ -26,14 +29,24 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
     if (new_size <= old_size)
         return ptr;
 
-    new_ptr = malloc(new_size);
-    if (!new_ptr)
+    /* Allocate memory for new pointer */
+    ptr2 = malloc(new_size);
+    if (ptr2 == NULL)
         return NULL;
 
-    for (i = 0; i < old_size; i++)
-        *((char *)new_ptr + i) = *((char *)ptr + i);
+    /* Copy data from old block to new */
+    size = (old_size < new_size) ? old_size : new_size;
+
+    /* Cast memory pointers to char pointers to manipulate individual chars */
+    char_ptr1 = (char *)ptr;
+    char_ptr2 = (char *)ptr2;
+
+    for (i = 0; i < size; i++)
+    {
+        char_ptr2[i] = char_ptr1[i];
+    }
 
     free(ptr);
 
-    return new_ptr;
+    return ptr2;
 }
