2dd2f236023c58a0b27d21be2818f585ead33182
Nancy <nancykubania2@gmail.com>
2023-08-30 12:04:19 +0000

    delete a node at specofied position

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,43 +1,36 @@
 #include "lists.h"
 
 /**
-  * delete_nodeint_at_index - Function that deletes a node
-  * @head: Pointer pointing to the pointer of the list
-  * @index: Index of the node that should be deleted.
-  * Return: 1 if it succeeded, -1 if it failed
-  */
-
+ * delete_nodeint_at_index - Function that deletes a node
+ * @head: Pointer pointing to the pointer of the list
+ * @index: Index of the node that should be deleted.
+ * Return: 1 if it succeeded, -1 if it failed
+ */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	listint_t *nextnode, *temp;
-	unsigned int i = 0;
+	listint_t *temp, *prev;
+	unsigned int i;
 
 	if (*head == NULL)
-	{
-		return (-1);
-	}
+	return (-1);
 
 	temp = *head;
+	prev = NULL;
 
-	while (i < index && temp != NULL)
+	for (i = 0; temp != NULL && i < index; i++)
 	{
+		prev = temp;
 		temp = temp->next;
-		i++;
-	}
-	if (temp == NULL)
-	{
-		return (-1);
 	}
 
-	if (index == 0)
-	{
+	if (temp == NULL)
+		return (-1);
+	if (prev == NULL)
 		*head = temp->next;
-	}
 	else
-	{
-		nextnode = temp->next;
-		temp->next = nextnode->next;
-		free(nextnode);
-	}
+		prev->next = temp->next;
+
+	free(temp);
+
 	return (1);
 }
