0bca4dd5e1b3c136aca448a28f3733f02d11464a
Nancy <nancykubania2@gmail.com>
2023-09-04 17:43:40 +0000

    appends text at the end of a file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,7 +17,8 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-	for (l = 0; text_content[l]; l++)
+	for (l = 0; text_content[l];)
+		l++;
 	}
 
 	file = open(filename, O_WRONLY | O_APPEND);
