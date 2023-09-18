a8d2dc6431084cd81f19cd07b76f4495d5d77b98
Nancy <nancykubania2@gmail.com>
2023-09-18 02:22:46 -0700

    compare string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,17 +13,17 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-    int i = 0;
+	 int i = 0;
 
-    while (s1[i] != '\0' && s2[i] != '\0')
-    {
-        if ((unsigned char)s1[i] != (unsigned char)s2[i])
-        {
-            return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-        }
-        i++;
-    }
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+	if ((unsigned char)s1[i] != (unsigned char)s2[i])
+	{
+		return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+	}
+	i++;
+	}
 
-    return 0;
+	return (0);
 }
 
