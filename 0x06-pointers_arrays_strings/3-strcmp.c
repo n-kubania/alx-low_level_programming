ef79fc5d22f343fae448471bf0d2ad058288ee83
Nancy <nancykubania2@gmail.com>
2023-09-18 02:16:04 -0700

    compare string

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,22 +3,27 @@
 #include <string.h>
 
 /**
- * _strcmp - compare two lines
- * @s1:first line to be compared
- * @s2:second line to be compared
- * Return: always o (success)
+ * _strcmp - compare two strings
+ * @s1: first string to be compared
+ * @s2: second string to be compared
+ * Return:
+ *   - Less than 0 if s1 is less than s2
+ *   - 0 if s1 is equal to s2
+ *   - Greater than 0 if s1 is greater than s2
  */
-
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+    int i = 0;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
-	{
-		if (s1[i] != s2[i])
-		{
-		return (s1[i] - s2[i]);
-		}
-	}
-	return (0);
+    while (s1[i] != '\0' && s2[i] != '\0')
+    {
+        if ((unsigned char)s1[i] != (unsigned char)s2[i])
+        {
+            return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+        }
+        i++;
+    }
+
+    return 0;
 }
+
